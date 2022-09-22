ff3a680c9a400d267162c493a80cd4e7edba2dfe
Guilherme Camboim <camboimguilherme@gmail.com>
2022-09-22 10:32:32 -0300

    Adicionando summary na aplicaçao.

--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -6,7 +6,23 @@ import { TransactionsContext } from "../../context/TransactionsContext";
 export function Summary(){
 const {transactions} = useContext(TransactionsContext)
 
-console.log(transactions)
+const summary = transactions.reduce(
+  (acc, transaction) => {
+    if (transaction.type === 'income'){
+      acc.income += transaction.price
+      acc.total += transaction.price
+    } else {
+      acc.outcome += transaction.price
+      acc.total -= transaction.price
+    }
+    return acc
+  }, 
+  {
+    income: 0,
+    outcome: 0,
+    total: 0,
+  }
+)
 
   return(
     <SummaryContainer>
@@ -16,7 +32,7 @@ console.log(transactions)
           <ArrowCircleUp size={32} color="#00B37E"/>
 
         </header>
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.income}</strong>
       </SummaryCard>
 
       <SummaryCard>
@@ -24,7 +40,7 @@ console.log(transactions)
           <span>Saídas</span>
           <ArrowCircleDown size={32} color="#F75A68"/>
         </header>
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.outcome}</strong>
       </SummaryCard>
 
       <SummaryCard variant="green">
@@ -34,7 +50,7 @@ console.log(transactions)
 
         </header>
 
-        <strong>R$ 17.400,00</strong>
+        <strong>{summary.total}</strong>
       </SummaryCard>
     </SummaryContainer>
   )
